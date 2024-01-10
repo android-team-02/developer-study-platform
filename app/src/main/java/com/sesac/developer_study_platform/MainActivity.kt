84cfa445eba1763bff8ce7efb5be371ef14342d3
nuyhus-m <minsh2344@gmail.com>
2024-01-10 18:16:28 +0900

    Feat: 스플래시 화면 구현

--- app/src/main/java/com/sesac/developer_study_platform/MainActivity.kt
+++ app/src/main/java/com/sesac/developer_study_platform/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.sesac.developer_study_platform
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var splashScreen: SplashScreen
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        splashScreen = installSplashScreen()
+        startSplash()
         setContentView(R.layout.activity_main)
+    }
+
+    private fun startSplash() {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            ObjectAnimator.ofPropertyValuesHolder(splashScreenView.iconView).run {
+                interpolator = AnticipateInterpolator()
+                duration = 1000L
+                doOnEnd {
+                    splashScreenView.remove()
+                }
+                start()
+            }
+        }
     }
 }
