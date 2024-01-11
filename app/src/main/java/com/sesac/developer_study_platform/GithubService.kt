0a17f92567b7415b3093736e94ae581f48ebce57
nuyhus-m <minsh2344@gmail.com>
2024-01-12 02:04:12 +0900

    Feat: 깃허브 api 이용하여 유저 정보 가져오기 구현

---
+++ app/src/main/java/com/sesac/developer_study_platform/GithubService.kt
@@ -0,0 +1,34 @@
+package com.sesac.developer_study_platform
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+interface GithubService {
+
+    @Headers("Accept: application/vnd.github+json")
+    @GET("users/{username}")
+    suspend fun getUserInfo(
+        @Path("username") userName : String,
+        @Header("Authorization") authorization : String
+    ) : User
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+
+        fun create(): GithubService {
+            val jsonConfig = Json { ignoreUnknownKeys = true }
+            val contentType = "application/json".toMediaType()
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(jsonConfig.asConverterFactory(contentType))
+                .build()
+                .create(GithubService::class.java)
+        }
+    }
+}
--- app/src/main/java/com/sesac/developer_study_platform/LoginFragment.kt
+++ app/src/main/java/com/sesac/developer_study_platform/LoginFragment.kt
@@ -7,11 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.google.firebase.auth.OAuthCredential
 import com.google.firebase.auth.OAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.sesac.developer_study_platform.databinding.FragmentLoginBinding
+import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment() {
 
@@ -46,6 +48,7 @@ class LoginFragment : Fragment() {
                     val userId = it.additionalUserInfo?.profile?.get("login").toString()
                     val accessToken = "Bearer ${(it.credential as? OAuthCredential)?.accessToken.toString()}"
                     saveUserIdAndAccessToken(userId, accessToken)
+                    getUserInfo(userId, accessToken)
                 }
                 .addOnFailureListener {
                     Log.d("Login", "Error : $it")
@@ -55,10 +58,23 @@ class LoginFragment : Fragment() {
 
     private fun saveUserIdAndAccessToken(userId: String, accessToken: String) {
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
-        with (sharedPref.edit()) {
+        with(sharedPref.edit()) {
             putString("userId", userId)
             putString("accessToken", accessToken)
             apply()
+        }
+    }
+
+    private fun getUserInfo(userId: String, accessToken: String) {
+        lifecycleScope.launch {
+            val githubService = GithubService.create()
+            kotlin.runCatching {
+                githubService.getUserInfo(userId, accessToken)
+            }.onSuccess {
+                Log.d("LoginFragment", "$it")
+            }.onFailure {
+                Log.e("LoginFragment", it.message ?: "error occurred.")
+            }
         }
     }
 
