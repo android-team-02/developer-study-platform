284ffd1f6ed4c8eacf18229a73c81e16118d1249
YuGyeong98 <kuk3097@naver.com>
2024-01-11 19:26:19 +0900

    Feat: main 액티비티 navigation 적용

--- app/src/main/java/com/sesac/developer_study_platform/MainActivity.kt
+++ app/src/main/java/com/sesac/developer_study_platform/MainActivity.kt
@@ -2,10 +2,21 @@ package com.sesac.developer_study_platform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.sesac.developer_study_platform.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnv.setupWithNavController(navController)
     }
 }
