64f93461819f15892d66431079170a8d8af45217
YuGyeong98 <kuk3097@naver.com>
2024-01-12 10:37:04 +0900

    Feat: 스터디 service 인터페이스 구현

---
+++ app/src/main/java/com/sesac/developer_study_platform/StudyService.kt
@@ -0,0 +1,26 @@
+package com.sesac.developer_study_platform
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.http.GET
+
+interface StudyService {
+
+    @GET("studies.json")
+    suspend fun getStudyList(): List<Study>
+
+    companion object {
+        private const val BASE_URL = BuildConfig.FIREBASE_BASE_URL
+        private val contentType = "application/json".toMediaType()
+
+        fun create(): StudyService {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(Json.asConverterFactory(contentType))
+                .build()
+                .create(StudyService::class.java)
+        }
+    }
+}
