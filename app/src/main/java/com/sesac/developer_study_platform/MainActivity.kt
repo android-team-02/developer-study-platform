87b906bb706925b3c3b8d6781f93d28d6da844e5
0se0 <030ggong@gmail.com>
2024-01-13 00:58:11 +0900

    Feat: navHostFragment 연결

--- app/src/main/java/com/sesac/developer_study_platform/MainActivity.kt
+++ app/src/main/java/com/sesac/developer_study_platform/MainActivity.kt
@@ -1,13 +1,23 @@
-package com.sesac.developer_study_platform
+package com.sesac.developer_study_platform // Replace with your actual package name
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.sesac.developer_study_platform.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+
     }
+
 }
-
-
