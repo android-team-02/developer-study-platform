ba19753213b9a2efe68825836389c0225abaa845
YuGyeong98 <kuk3097@naver.com>
2024-01-13 12:28:30 +0900

    Feat: 리사이클러뷰 어댑터 및 ItemDecoration 적용

--- app/src/main/java/com/sesac/developer_study_platform/CategoryFragment.kt
+++ app/src/main/java/com/sesac/developer_study_platform/CategoryFragment.kt
@@ -1,17 +1,23 @@
 package com.sesac.developer_study_platform
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.sesac.developer_study_platform.databinding.FragmentCategoryBinding
+import kotlinx.coroutines.launch
 
 class CategoryFragment : Fragment() {
 
     private var _binding: FragmentCategoryBinding? = null
     private val binding get() = _binding!!
     private lateinit var category: String
+    private val categoryAdapter = CategoryAdapter(object : StudyClickListener {
+        override fun onClick(study: Study) {}
+    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +35,33 @@ class CategoryFragment : Fragment() {
     ): View {
         _binding = FragmentCategoryBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setCategoryAdapter()
+        loadStudyList()
+    }
+
+    private fun setCategoryAdapter() {
+        binding.rvStudyList.adapter = categoryAdapter
+        binding.rvStudyList.addItemDecoration(
+            GridSpaceItemDecoration(resources.getDimensionPixelSize(R.dimen.space_median))
+        )
+    }
+
+    private fun loadStudyList() {
+        val service = StudyService.create()
+        lifecycleScope.launch {
+            kotlin.runCatching {
+                service.getStudyList(category)
+            }.onSuccess {
+                categoryAdapter.submitList(it)
+            }.onFailure {
+                Log.e("CategoryFragment", it.message ?: "error occurred.")
+            }
+        }
     }
 
     override fun onDestroyView() {
