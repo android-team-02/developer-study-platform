7bcf234635007d7ca460598bf01fe24087e147ba
YuGyeong98 <kuk3097@naver.com>
2024-01-13 12:20:54 +0900

    Feat: category enum class 구현

---
+++ app/src/main/java/com/sesac/developer_study_platform/Category.kt
@@ -0,0 +1,10 @@
+package com.sesac.developer_study_platform
+
+enum class Category(val resId: Int) {
+    ANDROID(R.string.all_android),
+    IOS(R.string.all_ios),
+    FRONTEND(R.string.all_front_end),
+    BACKEND(R.string.all_back_end),
+    AI(R.string.all_ai),
+    ETC(R.string.all_etc),
+}
