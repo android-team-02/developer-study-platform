da44ac18326a9e3fe2192b2d1a28b664264c3f5e
YuGyeong98 <kuk3097@naver.com>
2024-01-13 13:14:42 +0900

    Design: 리사이클러뷰 아이템 간격 수정

--- app/src/main/java/com/sesac/developer_study_platform/GridSpaceItemDecoration.kt
+++ app/src/main/java/com/sesac/developer_study_platform/GridSpaceItemDecoration.kt
@@ -16,19 +16,26 @@ class GridSpaceItemDecoration(private val space: Int) : RecyclerView.ItemDecorat
 
         val position = parent.getChildAdapterPosition(view)
         val column = position % 2 + 1
+        val itemCount = state.itemCount
 
         if (position < 2) {
             outRect.top = space
         } else {
-            outRect.top = space / 2
+            outRect.top = space / 4
         }
+
         if (column == 1) {
             outRect.left = space
-            outRect.right = space / 2
+            outRect.right = space / 4
         } else {
             outRect.right = space
-            outRect.left = space / 2
+            outRect.left = space / 4
         }
-        outRect.bottom = space / 2
+
+        if (position >= itemCount - 2 && position < itemCount) {
+            outRect.bottom = space
+        } else {
+            outRect.bottom = space / 4
+        }
     }
 }
