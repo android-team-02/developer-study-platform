a2fdd6ad57892048eda2ad39e2c1c172e31a8f23
0se0 <030ggong@gmail.com>
2024-01-13 15:44:48 +0900

    Feat: id 요청

--- app/src/main/java/com/sesac/developer_study_platform/StudyService.kt
+++ app/src/main/java/com/sesac/developer_study_platform/StudyService.kt
@@ -5,11 +5,15 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface StudyService {
 
     @GET("studies.json")
     suspend fun getStudyList(): List<Study>
+
+    @GET("studies/{id}.json")
+    suspend fun getStudyDetail(@Path("id") index: String): Study
 
     companion object {
         private const val BASE_URL = BuildConfig.FIREBASE_URL
