4fdf665e0ff9bbd7ebf097ee7690fbcb213e924d
YuGyeong98 <kuk3097@naver.com>
2024-01-13 12:32:11 +0900

    Feat: viewpager2, tablayout 적용

---
+++ app/src/main/java/com/sesac/developer_study_platform/SearchCategoryFragment.kt
@@ -0,0 +1,46 @@
+package com.sesac.developer_study_platform
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import com.sesac.developer_study_platform.databinding.FragmentSearchCategoryBinding
+
+class SearchCategoryFragment : Fragment() {
+
+    private var _binding: FragmentSearchCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setViewPager()
+    }
+
+    private fun setViewPager() {
+        binding.vp.adapter = ViewPagerAdapter(this)
+        attachTabLayout()
+    }
+
+    private fun attachTabLayout() {
+        TabLayoutMediator(binding.tl, binding.vp) { tab, position ->
+            tab.text = getString(Category.entries[position].resId)
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
