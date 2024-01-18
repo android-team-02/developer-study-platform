0e36815b14bb91c07925d829670e62a10f1a50f6
YuGyeong98 <kuk3097@naver.com>
2024-01-18 16:41:59 +0900

    Feat: message 데이터 클래스 구현

---
+++ app/src/main/java/com/sesac/developer_study_platform/data/Message.kt
@@ -0,0 +1,23 @@
+package com.sesac.developer_study_platform.data
+
+import android.os.Build
+import kotlinx.serialization.Serializable
+import java.time.LocalDateTime
+import java.util.Date
+
+@Serializable
+data class Message(
+    val uid: String?,
+    val userId: String?,
+    val userImage: String?,
+    val isAdmin: Boolean,
+    val message: String,
+    val totalMemberCount: Int,
+    val readUsers: Map<String?, Boolean>,
+) {
+    val timestamp = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        LocalDateTime.now().toString()
+    } else {
+        Date().toString()
+    }
+}
