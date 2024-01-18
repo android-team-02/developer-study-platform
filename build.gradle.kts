09f672109f0e07466ad856f5a37d7176ddc3fcdf
nuyhus-m <minsh2344@gmail.com>
2024-01-18 16:11:43 +0900

    Chore: Room 관련 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
+    id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false
 }
