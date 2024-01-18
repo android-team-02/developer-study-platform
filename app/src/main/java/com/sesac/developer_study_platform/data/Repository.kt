8c9f8d2dc52e915adc29c4f3c8661df9ec6214fb
0se0 <030ggong@gmail.com>
2024-01-19 00:04:10 +0900

    Feat: github api에 맞는 프로퍼티 이름으로 수정

--- app/src/main/java/com/sesac/developer_study_platform/data/Repository.kt
+++ app/src/main/java/com/sesac/developer_study_platform/data/Repository.kt
@@ -6,8 +6,8 @@ import kotlinx.serialization.Serializable
 data class Repository(
     val name: String,
     val language: String?,
-    val starsCount: Int = 0,
-    val forksCount: Int = 0,
-    val issuesCount: Int = 0,
-    val createdAt: String = "",
+    val stargazers_count: Int,
+    val forks_count: Int,
+    val open_issues_count: Int,
+    val created_at: String,
 )
