0e9a502a262c977a06b961a0620f16db4fe53319
nuyhus-m <minsh2344@gmail.com>
2024-01-18 19:08:56 +0900

    Feat: 로컬 데이터베이스에서 스터디 목록 가져오기 및 어댑터 적용

---
+++ app/src/main/java/com/sesac/developer_study_platform/ui/bookmark/BookmarkFragment.kt
@@ -0,0 +1,48 @@
+package com.sesac.developer_study_platform.ui.bookmark
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.sesac.developer_study_platform.StudyApplication
+import com.sesac.developer_study_platform.databinding.FragmentBookmarkBinding
+import com.sesac.developer_study_platform.ui.BookmarkClickListener
+import kotlinx.coroutines.launch
+
+class BookmarkFragment : Fragment() {
+    private var _binding: FragmentBookmarkBinding? = null
+    private val binding get() = _binding!!
+    private val bookmarkAdapter = BookmarkAdapter(object : BookmarkClickListener {
+        override fun onClick(sid: String) {
+            // 상세 화면으로 이동
+        }
+    })
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvStudy.adapter = bookmarkAdapter
+        loadStudyList()
+    }
+
+    private fun loadStudyList() {
+        lifecycleScope.launch {
+            bookmarkAdapter.submitList(StudyApplication.bookmarkDao.getAllBookmarkStudy())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
