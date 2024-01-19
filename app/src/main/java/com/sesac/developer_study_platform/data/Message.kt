36c9c3aec66790f062e2c1329b8e4f47acefe9eb
YuGyeong98 <kuk3097@naver.com>
2024-01-19 22:32:40 +0900

    Feat: message 데이터 클래스 구조 변경

--- app/src/main/java/com/sesac/developer_study_platform/data/Message.kt
+++ app/src/main/java/com/sesac/developer_study_platform/data/Message.kt
@@ -1,6 +1,7 @@
 package com.sesac.developer_study_platform.data
 
 import android.os.Build
+import com.sesac.developer_study_platform.ui.message.ViewType
 import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
 import java.util.Date
@@ -8,11 +9,14 @@ import java.util.Date
 @Serializable
 data class Message(
     val uid: String?,
+    val sid: String,
     val studyUser: StudyUser?,
     val isAdmin: Boolean,
     val message: String,
     val totalMemberCount: Int,
     val readUsers: Map<String?, Boolean>,
+    val type: ViewType,
+    val images: List<String>? = null,
 ) {
     val timestamp = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         LocalDateTime.now().toString()
