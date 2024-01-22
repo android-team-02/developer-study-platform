ce1f4404a7af7a3614a4194bbca5d46516594b5d
YuGyeong98 <kuk3097@naver.com>
2024-01-22 11:57:06 +0900

    Remove: 중복 라이브러리 삭제
    Co-authored-by: nuyhus-m <minsh2344@gmail.com>
    Co-authored-by: AnMyungwoo94 <anmy121212@gmail.com>
    Co-authored-by: 0se0 <030ggong@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,10 +75,4 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
