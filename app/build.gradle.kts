8382714738770d61e21c7c2c01f17b5b5e73369a
AnMyungwoo94 <anmy121212@gmail.com>
2024-01-28 18:45:10 +0900

    Chore: 머터리얼 캘린더뷰 종속성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,5 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("com.github.prolificinteractive:material-calendarview:2.0.1")
 }
