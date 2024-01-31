60b299fc67fbf7542bf3dfd0be773eab8aa2d4eb
YuGyeong98 <kuk3097@naver.com>
2024-01-31 12:02:24 +0900

    Feat: databinding 적용

--- app/src/main/java/com/sesac/developer_study_platform/ui/BindingAdapters.kt
+++ app/src/main/java/com/sesac/developer_study_platform/ui/BindingAdapters.kt
@@ -1,16 +1,26 @@
 package com.sesac.developer_study_platform.ui
 
+import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.BindingAdapter
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import com.google.firebase.storage.storage
 import com.sesac.developer_study_platform.data.Study
+import com.sesac.developer_study_platform.data.UserStudy
 import com.sesac.developer_study_platform.util.formatYearMonthDay
+import com.sesac.developer_study_platform.util.setImage
+
+@BindingAdapter("dayTimeList")
+fun setDayTimeList(view: TextView, dayList: List<String>?) {
+    view.text = dayList?.joinToString("\n")
+}
 
 @BindingAdapter("dayList")
 fun setDayList(view: TextView, dayList: List<String>?) {
-    view.text = dayList?.joinToString("\n")
+    view.text = dayList?.joinToString(", ") { it.split(" ").first() }
 }
 
 @BindingAdapter("isEnabled")
@@ -19,5 +29,16 @@ fun setEnabled(view: AppCompatButton, study: Study?) {
         view.isEnabled = !(formatYearMonthDay() > study.endDate
                 || study.members.count() == study.totalMemberCount
                 || study.banUsers.containsKey(Firebase.auth.uid))
+    }
+}
+
+@BindingAdapter("image")
+fun loadImage(view: ImageView, study: UserStudy) {
+    val storageRef = Firebase.storage.reference
+    val imageRef = storageRef.child("${study.sid}/${study.image}")
+    imageRef.downloadUrl.addOnSuccessListener {
+        view.setImage(it.toString())
+    }.addOnFailureListener {
+        Log.e("loadImage", it.message ?: "error occurred.")
     }
 }
