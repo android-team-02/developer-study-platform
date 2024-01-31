7c22285ff355b6ea20d1e3285f2b562ed278eb8f
YuGyeong98 <kuk3097@naver.com>
2024-01-31 11:57:32 +0900

    Feat: userstudy 리스트 가져오는 함수 레포지토리에 추가

--- app/src/main/java/com/sesac/developer_study_platform/data/source/remote/StudyRepository.kt
+++ app/src/main/java/com/sesac/developer_study_platform/data/source/remote/StudyRepository.kt
@@ -2,6 +2,7 @@ package com.sesac.developer_study_platform.data.source.remote
 
 import com.sesac.developer_study_platform.data.Study
 import com.sesac.developer_study_platform.data.StudyUser
+import com.sesac.developer_study_platform.data.UserStudy
 
 class StudyRepository {
 
@@ -13,5 +14,9 @@ class StudyRepository {
 
     suspend fun getUserById(uid: String): StudyUser {
         return studyService.getUserById(uid)
+    }
+
+    suspend fun getUserStudyList(uid: String?): Map<String, UserStudy> {
+        return studyService.getUserStudyList(uid)
     }
 }
