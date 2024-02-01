b2844d8f255e50ee03ae4f653a732dcd9f9b3f19
nuyhus-m <minsh2344@gmail.com>
2024-02-02 02:09:14 +0900

    Remove: TypeCoverter 안 사용하는 함수 삭제

--- app/src/main/java/com/sesac/developer_study_platform/data/Converters.kt
+++ app/src/main/java/com/sesac/developer_study_platform/data/Converters.kt
@@ -1,22 +1,8 @@
 package com.sesac.developer_study_platform.data
 
 import androidx.room.TypeConverter
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 class Converters {
-
-    @TypeConverter
-    fun stringToUserStudyList(value: String?): List<UserStudy>? {
-        return value?.let {
-            Json.decodeFromString(it)
-        }
-    }
-
-    @TypeConverter
-    fun userStudyListToString(userStudyList: List<UserStudy>?): String {
-        return Json.encodeToString(userStudyList)
-    }
 
     @TypeConverter
     fun stringToStringList(value: String?): List<String>? {
