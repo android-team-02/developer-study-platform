5734c1e75c45031fe4910da65e299cef0c391978
nuyhus-m <minsh2344@gmail.com>
2024-02-03 23:02:04 +0900

    Feat: 북마크 화면 프래그먼트에 뷰모델, 데이터 바인딩 적용

--- app/src/main/java/com/sesac/developer_study_platform/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/sesac/developer_study_platform/ui/bookmark/BookmarkFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.sesac.developer_study_platform.EventObserver
 import com.sesac.developer_study_platform.R
-import com.sesac.developer_study_platform.StudyApplication.Companion.bookmarkDao
 import com.sesac.developer_study_platform.databinding.FragmentBookmarkBinding
 import com.sesac.developer_study_platform.ui.common.SpaceItemDecoration
 import com.sesac.developer_study_platform.ui.common.StudyClickListener
@@ -24,21 +26,39 @@ class BookmarkFragment : Fragment() {
             findNavController().navigate(action)
         }
     })
+    private val viewModel by viewModels<BookmarkViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bookmark, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setBackButton()
+        setNavigation()
         setBookmarkAdapter()
         loadStudyList()
+    }
+
+    private fun setBackButton() {
+        binding.toolbar.setNavigationOnClickListener {
+            viewModel.moveToBack()
+        }
+    }
+
+    private fun setNavigation() {
+        viewModel.moveToBackEvent.observe(
+            viewLifecycleOwner,
+            EventObserver {
+                findNavController().popBackStack()
+            }
+        )
     }
 
     private fun setBookmarkAdapter() {
@@ -50,14 +70,20 @@ class BookmarkFragment : Fragment() {
 
     private fun loadStudyList() {
         lifecycleScope.launch {
-            val bookmarkStudyList = bookmarkDao.getAllBookmarkStudy()
-            if (bookmarkStudyList.isNotEmpty()) {
-                bookmarkAdapter.submitList(bookmarkStudyList)
-            } else {
-                binding.rvStudyList.visibility = View.GONE
-                binding.groupNoData.visibility = View.VISIBLE
-            }
+            viewModel.loadStudyList()
         }
+        viewModel.bookmarkStudyListEvent.observe(
+            viewLifecycleOwner,
+            EventObserver {
+                bookmarkAdapter.submitList(it)
+            }
+        )
+        viewModel.emptyStudyListEvent.observe(
+            viewLifecycleOwner,
+            EventObserver {
+                binding.isBookmarkStudyListEmpty = it
+            }
+        )
     }
 
     override fun onDestroyView() {
