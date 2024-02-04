168d033b25888e627235da3b33ec27be0acea7c2
AnMyungwoo94 <anmy121212@gmail.com>
2024-02-05 00:18:46 +0900

    Feat: Repository 추가

--- app/src/main/java/com/sesac/developer_study_platform/data/source/remote/StudyRepository.kt
+++ app/src/main/java/com/sesac/developer_study_platform/data/source/remote/StudyRepository.kt
@@ -2,7 +2,9 @@ package com.sesac.developer_study_platform.data.source.remote
 
 import com.sesac.developer_study_platform.data.Study
 import com.sesac.developer_study_platform.data.StudyUser
+import com.sesac.developer_study_platform.data.User
 import com.sesac.developer_study_platform.data.UserStudy
+import retrofit2.http.Body
 
 class StudyRepository {
 
@@ -18,5 +20,13 @@ class StudyRepository {
 
     suspend fun getUserStudyList(uid: String?): Map<String, UserStudy> {
         return studyService.getUserStudyList(uid)
+    }
+
+    suspend fun putStudy(sid: String, study: Study) {
+        return studyService.putStudy(sid, study)
+    }
+
+    suspend fun putUserStudy(uid: String, sid: String, userStudy: UserStudy) {
+        return studyService.putUserStudy(uid, sid, userStudy)
     }
 }
