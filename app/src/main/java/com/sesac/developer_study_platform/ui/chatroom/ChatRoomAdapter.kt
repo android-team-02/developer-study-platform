958056406ab236b83c01ba5574f4e943863ba7d5
YuGyeong98 <kuk3097@naver.com>
2024-02-05 21:51:02 +0900

    Feat: 어댑터에 databinding 적용

--- app/src/main/java/com/sesac/developer_study_platform/ui/chatroom/ChatRoomAdapter.kt
+++ app/src/main/java/com/sesac/developer_study_platform/ui/chatroom/ChatRoomAdapter.kt
@@ -1,19 +1,18 @@
 package com.sesac.developer_study_platform.ui.chatroom
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import com.sesac.developer_study_platform.R
 import com.sesac.developer_study_platform.data.ChatRoom
 import com.sesac.developer_study_platform.data.UserStudy
 import com.sesac.developer_study_platform.databinding.ItemChatRoomBinding
 import com.sesac.developer_study_platform.ui.common.StudyClickListener
-import com.sesac.developer_study_platform.util.formatTime
-import com.sesac.developer_study_platform.util.setImage
 
 class ChatRoomAdapter(private val clickListener: StudyClickListener) :
     ListAdapter<Pair<UserStudy, ChatRoom>, ChatRoomAdapter.ChatRoomViewHolder>(diffUtil) {
@@ -30,31 +29,18 @@ class ChatRoomAdapter(private val clickListener: StudyClickListener) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(pair: Pair<UserStudy, ChatRoom>, clickListener: StudyClickListener) {
-            // TODO 마지막 메시지가 이미지 일 때, 텍스트 일 때 나눠서 처리
-            // TODO 마지막 메시지 타임스탬프가 오늘이 안지났으면 시간, 지났으면 날짜로 처리
-            val study = pair.first
-            val chatRoom = pair.second
-            binding.ivStudyImage.setImage(study.image)
-            binding.tvStudyName.text = study.name
-            binding.tvLastMessage.text = chatRoom.lastMessage.message
-            binding.tvLastMessageTime.text = chatRoom.lastMessage.timestamp.formatTime()
-            val unreadCount = chatRoom.unreadUsers.getOrDefault(Firebase.auth.uid, 0)
-            if (unreadCount > 0) {
-                binding.tvUnreadCount.visibility = View.VISIBLE
-                binding.tvUnreadCount.text = unreadCount.toString()
-            } else {
-                binding.tvUnreadCount.visibility = View.GONE
-            }
-            itemView.setOnClickListener {
-                clickListener.onClick(study.sid)
-            }
+            binding.userStudy = pair.first
+            binding.chatRoom = pair.second
+            binding.unreadCount = pair.second.unreadUsers.getOrDefault(Firebase.auth.uid, 0)
+            binding.clickListener = clickListener
         }
 
         companion object {
             fun from(parent: ViewGroup): ChatRoomViewHolder {
                 return ChatRoomViewHolder(
-                    ItemChatRoomBinding.inflate(
+                    DataBindingUtil.inflate(
                         LayoutInflater.from(parent.context),
+                        R.layout.item_chat_room,
                         parent,
                         false
                     )
