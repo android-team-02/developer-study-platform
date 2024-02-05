d449474617c78de3609b83df4975e22a79bdbcad
YuGyeong98 <kuk3097@naver.com>
2024-02-05 21:50:32 +0900

    Feat: databinding 적용

--- app/src/main/java/com/sesac/developer_study_platform/ui/BindingAdapters.kt
+++ app/src/main/java/com/sesac/developer_study_platform/ui/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.sesac.developer_study_platform.ui
 
+import android.os.Build
 import android.util.Log
 import android.widget.ImageView
 import android.view.View
@@ -11,8 +12,12 @@ import com.google.firebase.auth.auth
 import com.google.firebase.storage.storage
 import com.sesac.developer_study_platform.data.Study
 import com.sesac.developer_study_platform.data.UserStudy
+import com.sesac.developer_study_platform.util.formatDate
+import com.sesac.developer_study_platform.util.formatTime
 import com.sesac.developer_study_platform.util.formatYearMonthDay
 import com.sesac.developer_study_platform.util.setImage
+import java.time.LocalDateTime
+import java.util.Date
 
 @BindingAdapter("dayTimeList")
 fun setDayTimeList(view: TextView, dayList: List<String>?) {
@@ -50,5 +55,22 @@ fun setVisibility(view: View, value: Boolean) {
         view.visibility = View.VISIBLE
     } else {
         view.visibility = View.GONE
+    }
+}
+
+@BindingAdapter("lastMessageTime")
+fun setLastMessageTime(view: TextView, timestamp: String) {
+    if (timestamp <= getToday()) {
+        view.text = timestamp.formatTime()
+    } else {
+        view.text = timestamp.formatDate()
+    }
+}
+
+fun getToday(): String {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        LocalDateTime.now().toString()
+    } else {
+        Date().toString()
     }
 }
