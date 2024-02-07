aac6d6a1fee346ba64a5c72d027e50fdd592d84e
0se0 <030ggong@gmail.com>
2024-02-07 14:18:04 +0900

    Feat: 다이얼로그 구현

---
+++ app/src/main/java/com/sesac/developer_study_platform/ui/detail/DetailDialogFragment.kt
@@ -0,0 +1,29 @@
+package com.sesac.developer_study_platform.ui.detail
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.sesac.developer_study_platform.R
+
+class DetailDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(requireActivity())
+        val view = requireActivity().layoutInflater.inflate(R.layout.dialog_detail, null)
+        builder.setView(view)
+        val dialog = builder.create()
+
+        view.findViewById<View>(R.id.btn_yes).setOnClickListener {
+            //채팅화면으로 이동
+            dialog.dismiss()
+        }
+
+        view.findViewById<View>(R.id.btn_no).setOnClickListener {
+            dialog.dismiss()
+        }
+
+        return dialog
+    }
+}
