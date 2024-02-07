9dbb000a7dcb455f5c6b3b0d1c7e9e2e1aa9c9ba
0se0 <030ggong@gmail.com>
2024-02-07 10:49:18 +0900

    Feat: BindingAdapter을 통한 이미지 로딩

---
+++ app/src/main/java/com/sesac/developer_study_platform/ui/profile/ImgBindingAdapter.kt
@@ -0,0 +1,14 @@
+package com.sesac.developer_study_platform.ui.profile
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imageUrl")
+fun ImageView.setImageUrl(url: String?) {
+    if (!url.isNullOrEmpty()) {
+        Glide.with(this.context)
+            .load(url)
+            .into(this)
+    }
+}
