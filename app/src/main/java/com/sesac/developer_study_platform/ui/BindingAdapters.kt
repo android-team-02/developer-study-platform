dbb0f58953ee788af5490f2330651a6f55b58e21
YuGyeong98 <kuk3097@naver.com>
2024-02-07 22:08:17 +0900

    Feat: binding adapter 채팅 관련 함수 구현

--- app/src/main/java/com/sesac/developer_study_platform/ui/BindingAdapters.kt
+++ app/src/main/java/com/sesac/developer_study_platform/ui/BindingAdapters.kt
@@ -7,12 +7,16 @@ import android.view.View
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.storage.storage
+import com.sesac.developer_study_platform.R
+import com.sesac.developer_study_platform.data.Message
 import com.sesac.developer_study_platform.data.Study
 import com.sesac.developer_study_platform.data.UserStudy
 import com.sesac.developer_study_platform.util.formatDate
+import com.sesac.developer_study_platform.util.formatSystemMessage
 import com.sesac.developer_study_platform.util.formatTime
 import com.sesac.developer_study_platform.util.formatYearMonthDay
 import com.sesac.developer_study_platform.util.setImage
@@ -72,5 +76,50 @@ fun getToday(): String {
         LocalDateTime.now().toString()
     } else {
         Date().toString()
+    }
+}
+
+@BindingAdapter("imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    if (!url.isNullOrEmpty()) {
+        Glide.with(view)
+            .load(url)
+            .centerCrop()
+            .into(view)
+    }
+}
+
+@BindingAdapter("previousMessage", "message")
+fun setSystemMessageVisibility(view: View, previousMessage: Message?, message: Message) {
+    if (previousMessage != null) {
+        if (previousMessage.timestamp.formatSystemMessage() < message.timestamp.formatSystemMessage()) {
+            view.visibility = View.VISIBLE
+        } else {
+            view.visibility = View.GONE
+        }
+        if (previousMessage.totalMemberCount != message.totalMemberCount) {
+            view.visibility = View.VISIBLE
+        } else {
+            view.visibility = View.GONE
+        }
+    } else {
+        view.visibility = View.VISIBLE
+    }
+}
+
+@BindingAdapter("previousMessageText", "messageText")
+fun setSystemMessage(view: TextView, previousMessage: Message?, message: Message) {
+    val messageTimestamp = message.timestamp.formatSystemMessage()
+    if (previousMessage != null) {
+        if (previousMessage.timestamp.formatSystemMessage() < messageTimestamp) {
+            view.text = messageTimestamp
+        }
+        if (previousMessage.totalMemberCount < message.totalMemberCount) {
+            view.text = view.context.getString(R.string.message_new_study_member)
+        } else if (previousMessage.totalMemberCount > message.totalMemberCount) {
+            view.text = view.context.getString(R.string.message_left_study_member)
+        }
+    } else {
+        view.text = messageTimestamp
     }
 }
