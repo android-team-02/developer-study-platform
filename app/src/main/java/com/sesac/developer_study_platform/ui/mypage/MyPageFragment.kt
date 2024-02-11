e35a835e956c571fdd6452d283726b0edd411631
YuGyeong98 <kuk3097@naver.com>
2024-02-11 22:53:40 +0900

    Feat: 마이 페이지 스터디 클릭 시 채팅방으로 이동

--- app/src/main/java/com/sesac/developer_study_platform/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sesac/developer_study_platform/ui/mypage/MyPageFragment.kt
@@ -108,22 +108,35 @@ class MyPageFragment : Fragment() {
     }
 
     private fun setNavigation() {
+        moveToBookmark()
+        moveToDialog()
+        moveToMessage()
+    }
+
+    private fun moveToBookmark() {
         viewModel.moveToBookmarkEvent.observe(
             viewLifecycleOwner,
             EventObserver {
                 findNavController().navigate(R.id.action_my_to_bookmark)
             }
         )
+    }
+
+    private fun moveToDialog() {
         viewModel.moveToDialogEvent.observe(
             viewLifecycleOwner,
             EventObserver {
                 // TODO 로그아웃 다이얼로그로 이동
             }
         )
+    }
+
+    private fun moveToMessage() {
         viewModel.moveToMessageEvent.observe(
             viewLifecycleOwner,
             EventObserver {
-                // TODO 채팅방으로 이동
+                val action = MyPageFragmentDirections.actionGlobalToMessage(it)
+                findNavController().navigate(action)
             }
         )
     }
