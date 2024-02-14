a107f03d047be5c05a8b353dba83cb5be1361115
nuyhus-m <minsh2344@gmail.com>
2024-02-14 14:39:00 +0900

    Feat: MyStudyDao에 getAllMyStudiesFlow 함수랑 deleteAllMyStudies 함수 구현

--- app/src/main/java/com/sesac/developer_study_platform/data/source/local/MyStudyDao.kt
+++ app/src/main/java/com/sesac/developer_study_platform/data/source/local/MyStudyDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.sesac.developer_study_platform.data.UserStudy
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MyStudyDao {
@@ -15,4 +16,10 @@ interface MyStudyDao {
 
     @Query("SELECT * FROM my_studies ORDER BY `sid` DESC")
     fun getAllMyStudies(): LiveData<List<UserStudy>>
+
+    @Query("SELECT * FROM my_studies ORDER BY `sid` DESC")
+    fun getAllMyStudiesFlow(): Flow<List<UserStudy>>
+
+    @Query("DELETE FROM my_studies")
+    suspend fun deleteAllMyStudies()
 }
