7596ed25bcb26f703bf79694b88d9a9adb63abee
nuyhus-m <minsh2344@gmail.com>
2024-02-14 16:34:39 +0900

    Fix: 웹뷰 구현 수정

--- app/src/main/java/com/sesac/developer_study_platform/ui/profile/WebViewFragment.kt
+++ app/src/main/java/com/sesac/developer_study_platform/ui/profile/WebViewFragment.kt
@@ -4,38 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.sesac.developer_study_platform.R
+import com.sesac.developer_study_platform.databinding.FragmentWebviewBinding
 
 class WebViewFragment : Fragment() {
 
+    private var _binding: FragmentWebviewBinding? = null
+    private val binding get() = _binding!!
     private val args by navArgs<WebViewFragmentArgs>()
-    private lateinit var webView: WebView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_webview, container, false)
+    ): View {
+        _binding = FragmentWebviewBinding.inflate(inflater, container, false)
+        return binding.root
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupWebView(view)
-    }
-
-    private fun setupWebView(view: View) {
-        webView = view.findViewById(R.id.webview)
-        webView.settings.javaScriptEnabled = true
-        webView.loadUrl(args.url)
+        binding.webview.loadUrl(args.url)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        webView.destroy()
+        _binding = null
+        binding.webview.destroy()
     }
-
 }
