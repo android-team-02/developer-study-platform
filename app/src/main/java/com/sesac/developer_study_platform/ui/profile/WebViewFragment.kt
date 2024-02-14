ae1b063f631539c6b5d36b98ae49fba02d30519a
nuyhus-m <minsh2344@gmail.com>
2024-02-14 17:58:18 +0900

    Feat: WebViewClient 구현

--- app/src/main/java/com/sesac/developer_study_platform/ui/profile/WebViewFragment.kt
+++ app/src/main/java/com/sesac/developer_study_platform/ui/profile/WebViewFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.sesac.developer_study_platform.databinding.FragmentWebviewBinding
@@ -24,6 +25,7 @@ class WebViewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.webview.webViewClient = WebViewClient()
         binding.webview.loadUrl(args.url)
     }
 
