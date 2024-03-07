c8b8db4dc488b0e331136fc8c5832a917fa0d7fb
YuGyeong98 <kuk3097@naver.com>
2024-03-08 00:12:47 +0900

    Feat: 스터디 참여 다이얼로그 화면 알림 권한 요청 구현

--- app/src/main/java/com/sesac/developer_study_platform/ui/detail/JoinStudyDialogFragment.kt
+++ app/src/main/java/com/sesac/developer_study_platform/ui/detail/JoinStudyDialogFragment.kt
@@ -1,10 +1,16 @@
 package com.sesac.developer_study_platform.ui.detail
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -12,14 +18,26 @@ import androidx.navigation.fragment.navArgs
 import com.sesac.developer_study_platform.EventObserver
 import com.sesac.developer_study_platform.R
 import com.sesac.developer_study_platform.data.UserStudy
+import com.sesac.developer_study_platform.data.source.local.FcmTokenRepository
 import com.sesac.developer_study_platform.databinding.DialogJoinStudyBinding
 
 class JoinStudyDialogFragment : DialogFragment() {
 
     private var _binding: DialogJoinStudyBinding? = null
     private val binding get() = _binding!!
-    private val viewModel by viewModels<JoinStudyDialogViewModel>()
+    private val viewModel by viewModels<JoinStudyDialogViewModel> {
+        JoinStudyDialogViewModel.create(FcmTokenRepository(requireContext()))
+    }
     private val args by navArgs<JoinStudyDialogFragmentArgs>()
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                updateStudyGroup()
+            } else {
+                Toast.makeText(context, getString(R.string.all_notification_info), Toast.LENGTH_SHORT).show()
+                viewModel.moveToMessage(args.study.sid)
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -62,7 +80,7 @@ class JoinStudyDialogFragment : DialogFragment() {
             viewModel.addUserStudyEvent.observe(
                 viewLifecycleOwner,
                 EventObserver {
-                    viewModel.moveToMessage(args.study.sid)
+                    askNotificationPermission()
                 }
             )
         }
@@ -74,6 +92,39 @@ class JoinStudyDialogFragment : DialogFragment() {
             EventObserver {
                 val action = JoinStudyDialogFragmentDirections.actionJoinStudyDialogToMessage(it)
                 findNavController().navigate(action)
+            }
+        )
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            when {
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    updateStudyGroup()
+                }
+
+                shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                    // TODO 권한 이유 다이얼로그
+                }
+
+                else -> {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+        } else {
+            updateStudyGroup()
+        }
+    }
+
+    private fun updateStudyGroup() {
+        viewModel.updateStudyGroup(args.study.sid)
+        viewModel.updateStudyGroupEvent.observe(
+            viewLifecycleOwner,
+            EventObserver {
+                viewModel.moveToMessage(args.study.sid)
             }
         )
     }
