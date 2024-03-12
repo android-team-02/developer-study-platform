121244b03b533ab3108b9bc1d5afa20a794c664c
YuGyeong98 <kuk3097@naver.com>
2024-03-12 17:35:03 +0900

    Chore: release 빌드시 디버깅 불가능 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
